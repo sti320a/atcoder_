13063928c80f6aee0e99dcb81d2cc91a9edd7580
Tomoaki Sugiayma <sti320a@gmail.com>
2019-05-27 22:56:58 +0900

    Update

--- b128.py
+++ b128.py
@@ -1,8 +1,18 @@
+from operator import itemgetter
+
 N = int(input())
 ins = [list(input().split()) for _ in range(N)]
 
-print(ins)
-
-# 市名で辞書式ソート
+res = []
+for i, row in enumerate(ins):
+    row = [i+1] + [row[0]] + [int(row[1])]
+    res.append(row)
 
 # 点数でソート
+res.sort(key=itemgetter(2), reverse=True)
+
+# 市名で辞書式ソート
+res.sort(key=itemgetter(1))
+
+for i in res:
+    print(i[0])
