efaaa149f6a81416ae32d60825799b0b2302e091
Tomoaki Sugiayma <sti320a@gmail.com>
2019-05-27 22:37:30 +0900

    Update

---
+++ a128.py
@@ -0,0 +1,3 @@
+stdin = list(map(int, input().split()))
+
+print(int((stdin[0]*3+stdin[1]) / 2))
---
+++ b128.py
@@ -0,0 +1,8 @@
+N = int(input())
+ins = [list(input().split()) for _ in range(N)]
+
+print(ins)
+
+# 市名で辞書式ソート
+
+# 点数でソート
