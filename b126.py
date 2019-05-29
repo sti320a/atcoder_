3c3acd167d1edd60fa56cfdc0e4cd7505c54d273
Tomoaki Sugiayma <sti320a@gmail.com>
2019-05-29 22:23:59 +0900

    Update

--- b126.py
+++ b126.py
@@ -4,21 +4,31 @@ aa = S[0:2]
 bb = S[2:4]
 
 
-def isYY(aa):
-    if 1 <= int(aa) and int(aa) <= 12:
+def isYY(yy):
+    if 0 <= int(yy) and int(yy) <= 99:
         return True
     else:
         return False
 
 
-def isXX(bb):
-    if 1 <= int(bb) and int(bb) <= 31:
+def isMM(mm):
+    if 1 <= int(mm) and int(mm) <= 12:
         return True
     else:
         return False
 
 
-if isYY(aa) and isXX(bb):
-    print(aa + bb)
+ans = []
+if isYY(aa) and isMM(bb):
+    # YYMM
+    ans.append('YYMM')
+if isMM(aa) and isYY(bb):
+    # MMYY
+    ans.append('MMYY')
 
-# TODO
+if len(ans) >= 2:
+    print('AMBIGUOUS')
+elif len(ans) == 1:
+    print(ans[0])
+else:
+    print('NA')
