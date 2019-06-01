c743b4ac80238ccba0a9111312df5245a19f6a1e
Tomoaki Sugiayma <sti320a@gmail.com>
2019-06-01 13:30:46 +0900

    Add test bitwise operation

---
+++ test_bit_ope.py
@@ -0,0 +1,23 @@
+state = 0b0000  # 初期状態
+
+sleep = 0b0001  # 眠り
+poison = 0b0010  # 毒
+palrysis = 0b0100  # 麻痺
+excitation = 0b1000  # 励起
+
+
+# 眠り状態か判別
+def isSleep(state):
+    if state & 0b0001:
+        return 'Sleep'
+    else:
+        return 'Not Sleep'
+
+
+# 眠り状態にする
+state = state | sleep
+print(isSleep(state))
+
+# 眠り状態を解く
+state = state & ~ sleep
+print(isSleep(state))
